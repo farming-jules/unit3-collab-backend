4c5d97900bb5aeeeea3e2e1e7c839edf7c57bddb
farming-jules <wyk.julian@gmail.com>
2021-07-13 15:03:16 +0800

    matches-update

--- controllers/api/my-matches/get-index.js
+++ controllers/api/my-matches/get-index.js
@@ -13,9 +13,45 @@ const apiMyMatchesIndex = async function(req, res) {
   let matchedLikes = await Like.findAll({ where: { TargetId: (currentUser.id), like: true, OwnerId: { [Op.in]: TargetIds} }, raw: true })
   matchedLikeIds = matchedLikes.map((x) => x.OwnerId)
 
-  let matches = await User.findAll({ where: { id: { [Op.in]:matchedLikeIds } }, raw: true })
+  let matches = await User.findAll({ where: { id: { [Op.in]:matchedLikeIds } }, raw: true,
+    include: [
+      {
+        association: User.OwnerLikes,
+        where: {
+          TargetId: (currentUser.id),
+          like: true,
+        },
+        attributes: []
+      },
+      {
+        association: User.UserImages,
+        attributes: ['image'],
+        limit: 1
+      }
+    ],
+    attributes: ['name'] })
 
   res.status(200).json(matches)
 }
 
 module.exports = [authenticateCurrentUserByToken('json'), apiMyMatchesIndex]
+
+
+// await User.findAll({
+//   include: [
+//     {
+//       association: User.OwnerLikes,
+//       where: {
+//         TargetId: (currentUser.id),
+//         like: true,
+//       },
+//       attributes: []
+//     },
+//     {
+//       association: User.UserImages,
+//       attributes: ['image'],
+//       limit: 1
+//     }
+//   ],
+//   attributes: ['name']
+// })
