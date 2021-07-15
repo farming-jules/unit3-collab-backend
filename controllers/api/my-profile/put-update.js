65490de1b44056ab54d3e1993a70902ce2041cc6
javadoge <ramechung@gmail.com>
2021-07-15 11:46:03 +0800

    fixed put my-profile saving UserImages associations

--- controllers/api/my-profile/put-update.js
+++ controllers/api/my-profile/put-update.js
@@ -22,60 +22,18 @@ const validation = [
   body('passion').isString().withMessage('Passion must be a String').notEmpty().withMessage('Passion cannot be empty'),
   body('lookingFor').isString().withMessage('Looking For must be a String').notEmpty().withMessage('Looking For cannot be empty'),
   body('location').isString().withMessage('Location must be a String').notEmpty().withMessage('Location cannot be empty'),
-  body('bio').isString().withMessage('bio must be a String').notEmpty().withMessage('bio cannot be empty'),
-  check('UserImages').custom((value, { req }) => {
-    const { body: { UserImages }} = req
-    const filesKeys = Object.keys(req.files || {})
-
-    const filesLength = filesKeys.length || 0
-    const imagesLength = (UserImages && UserImages.length) || 0
-
-    if (filesLength + imagesLength < 1) return false
-
-    return true
-  }).withMessage('You need to put at least 1 image')
+  body('bio').isString().withMessage('bio must be a String').notEmpty().withMessage('bio cannot be empty')
 ]
 
 const apiMyProfileUpdate = async function(req, res) {
   const { locals: { currentUser } } = res
-  const { body: userParams } = req
+  const { body: { UserImages, ...userParams } } = req
 
-  await currentUser.update(userParams, { fields: permittedParams })
-
-  if (userParams.UserImages && userParams.UserImages.length > 0) {
-    const images = await currentUser.getUserImages({ raw: true })
-    await UserImage.destroy({ where: { UserId: currentUser.id }})
-
-    if (userParams.UserImages && userParams.UserImages.length > 0) {
-      for (let i = 0; i < userParams.UserImages.length; i++) {
-        const { id } = userParams.UserImages[i]
-        const foundUserImage = images.find((image) => image.id == id)
-
-        if (foundUserImage) {
-          await currentUser.createUserImage(foundUserImage)
-        }
-      }
-    }
-  }
-
-  for (let key in (req.files || {})) {
-    const newFile = req.files[key][0]
-
-    await currentUser.createUserImage({ image: newFile.location })
-  }
-
-  await currentUser.reload()
+  await currentUser.update(userParams, {
+    fields: permittedParams
+  })
 
   res.status(200).json(currentUser)
 }
 
-const imageFields = [
-  { name: 'UserImages[0][image]', maxCount: 1 },
-  { name: 'UserImages[1][image]', maxCount: 1 },
-  { name: 'UserImages[2][image]', maxCount: 1 },
-  { name: 'UserImages[3][image]', maxCount: 1 },
-  { name: 'UserImages[4][image]', maxCount: 1 },
-  { name: 'UserImages[5][image]', maxCount: 1 }
-]
-
-module.exports = [authenticateCurrentUserByToken('json'), MulterParser.fields(imageFields), validation, checkValidation, apiMyProfileUpdate]
+module.exports = [authenticateCurrentUserByToken('json'), MulterParser.none(), validation, checkValidation, apiMyProfileUpdate]
